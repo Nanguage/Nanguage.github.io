b536e75fbe318ccbdcf2d0789728b353ec094097
nanguage <nanguage@yahoo.com>
2019-02-14 21:21:34 +0800

    update 2019年 02月 14日 星期四 21:21:34 CST

--- assets/js/001.js
+++ assets/js/001.js
@@ -4,7 +4,15 @@
 var max = document.getElementsByClassName("btn")[1];
 var min = document.getElementsByClassName("btn")[2];
 
+var status = "normal" // "normal", "maxmize"
+var explorer = document.getElementsByClassName("wrapper")[0]
+
 function maximize () {
+	// hide file explorer
+	var expl = document.getElementsByClassName("wrapper")[0]
+	expl.parentNode.removeChild(expl)
+
+	// change width
 	var post = document.getElementsByClassName("content")[0];
 	var cont = document.getElementsByClassName("post_content")[0];
 	var wid = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName("body")[0].clientWidth;
@@ -21,7 +29,18 @@ function maximize () {
 	}
 }
 
+function max_btn_click () {
+	if ( status == "normal" ) {
+		maximize()
+		status = "maxmize"
+	}
+}
+
 function minimize () {
+	// insert explorer
+	document.body.insertBefore(explorer, document.body.childNodes[0])
+
+	// change width
 	var post = document.getElementsByClassName("content")[0];
 	var cont = document.getElementsByClassName("post_content")[0];
 	var wid = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName("body")[0].clientWidth;
@@ -29,6 +48,13 @@ function minimize () {
 	if ( wid > 900 ) {
 		post.style.width = "800px";
 		cont.style.width = "98.5%";
+	}
+}
+
+function min_btn_click () {
+	if ( status == "maxmize" ) {
+		minimize()
+		status = "normal"
 	}
 }
 
